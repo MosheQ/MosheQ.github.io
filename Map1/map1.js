7235ebffef0b6c9befc27bd075e7f85f6e9f9d2b
User1 <user@example.com>
2020-04-12 14:55:13 -0500

    gjhjglkjujikuj

--- Map1/map1.js
+++ Map1/map1.js
@@ -1,11 +1,18 @@
 var MO_4046 = L.map('MO_4046').setView([32.18, -99.14], 4)
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png').addTo('MO_4046')
-var renameThisMarkerObject = L.marker([30.3947416,-91.1735687]).addTo('MO_4046')
-var renameThisPolygonObject = L.polygon([
+var Block_G = L.marker([30.3947416, -91.1735687]).addTo('MO_4046')
+var Armstrong_Apartment = L.polygon([
   [30.399766, -91.174437],
   [30.396898, -91.177514],
   [30.395058, -91.173975],
   [30.398310, -91.170756]
 ]).addTo('MO_4046')
+// create a red polyline from an array of LatLng points
+var latlngs = [  30.396898, -91.177514],
+  [30.395058, -91.173975],
+  [30.398310, -91.170756]
+]
+
+map.fitBounds(polyline.getBounds());
 renameThisPolygonObject.bindPopup('The Armstrong')
 renameThisMarkerObject.bindPopup('MO_s Apartment')
