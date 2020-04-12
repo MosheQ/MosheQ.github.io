bb71473b829f2c4af1d5233f27ba55319074afc5
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-04-12 18:56:48 -0500

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,16 +1,1 @@
-var mo4046 = L.map('mapdiv').setView([32.18, -99.14], 4)
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png').addTo(mo4046)
-var BlockG = L.marker([30.3947416, -91.1735687]).addTo(mo4046)
-var ArmstrongApartment = L.polygon([
-  [30.399766, -91.174437],
-  [30.396898, -91.177514],
-  [30.395058, -91.173975],
-  [30.398310, -91.170756]
-]).addTo(mo4046)
-// create a red polyline from an array of LatLng points
-var polyline1 = L.polyline([[30.397059, -91.174414], [30.416975, -91.171357]]).addTo(mo4046);
-mo4046.fitBounds(polyline1.getBounds());
 
-BlockG.bindPopup('The Armstrong')
-ArmstrongApartment.bindPopup('MO_s Apartment')
-polyline1.bindPopup('Flight of the crow')
