b7227456e09e56041efc475673f7346cd81c4457
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-04-12 19:38:39 -0500

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,4 +1,4 @@
-var MO4046 = L.map('mymap').setView([32.18, -99.14], 4)
+var MO4046 = L.map('mymap').setView([32.18, -99.14], 15)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(MO4046)
 var point1 = L.marker([30.3947416, -91.1735687]).addTo(MO4046)
 var polygon1 = L.polygon([
