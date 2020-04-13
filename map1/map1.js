ae36945ca1808c7d81e43fd761ad32762835c752
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-04-12 19:03:28 -0500

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,2 +1,3 @@
 var MO4046 = L.map('mymap').setView([32.18, -99.14], 4)
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png').addTo(MO4046)
+var point1 = L.marker([30.3947416, -91.1735687]).addTo(MO4046)
