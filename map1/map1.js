af52de132d06d2c9e0950898ac271fb37a54dc36
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-04-12 19:08:13 -0500

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,11 +1,11 @@
 var MO4046 = L.map('mymap').setView([32.18, -99.14], 4)
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png').addTo(MO4046)
 var point1 = L.marker([30.3947416, -91.1735687]).addTo(MO4046)
-var Polygon1 = L.polygon([
+var polygon1 = L.polygon([
   [31, -88],
   [35, -88],
   [32, -82]
 ]).addTo(MO4046)
 
-Polygon1.bindPopup('Your polygon popup text here.')
-Point1.bindPopup('Your marker popup text here.')
+polygon1.bindPopup('Your polygon popup text here.')
+point1.bindPopup('Your marker popup text here.')
