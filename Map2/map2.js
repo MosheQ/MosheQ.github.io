6e554479ac7cfb1531926193f51d4e424ad52f51
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-04-15 22:19:49 -0500

    Update map2.js

--- Map2/map2.js
+++ Map2/map2.js
@@ -2,13 +2,6 @@ var MO4046 = L.map('mymap').setView([32.18, -99.14], 5)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(MO4046)
 
 
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
-}).addTo(MO4046)
-
 
 L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
   layers: 'nexrad-n0r-900913',
@@ -16,3 +9,4 @@ L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(MO4046)
+
