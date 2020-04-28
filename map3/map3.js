e8490a4b87f517c869c790ec537d80c303dd998a
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-04-28 12:35:22 -0500

    Update map3.js

--- map3/map3.js
+++ map3/map3.js
@@ -1,43 +1,40 @@
-var map3 = L.map('mapdiv3').setView([39.163989, -102.023264], 5)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map3)
+// initialize the map
+var mymap3 = L.map('mapdiv3').setView([36.47431, -96.65782], 4)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap3)
+var gjsonUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 
+//////////////////////////////////////////////////////////////////////////////
+jQuery.getJSON(gjsonUrl, function(data) {
 
-var stdemographics = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
+  var styleFunction = function(feature) {
+    var medAge = feature.properties.MED_AGE // get the current state's Median Age attribute
+    var colorString = 'purple' // let the initial color be a darker green
+    
+        if (medAge < 25) {
+      colorString = 'red'
+    } // if the state's median age is less than the average, color it a lighter green
+    
+    if (medAge < 38) {
+      colorString = 'green'
+    } // if the state's median age is less than the average, color it a lighter green
+    return {
+      color: colorString, // use the color variable above for the value
+      weight: 1,
+      fillOpacity: 0.3
+    }
+  }
 
-jQuery.getJSON(stdemographics, function (us_state_demographics_ESRI_2010A) {
-  L.geoJSON(us_state_demographics_ESRI_2010A).addTo(map3)
+  var geojsonObject = {
+    style: styleFunction,
+  onEachFeature: onEachFeatureFunction
+  }
+  L.geoJSON(data, geojsonObject).addTo(mymap3)
 })
 
-
-
-//              |Happy|  
-//                       |Beautiful|
-L.geoJSON(data, { style: { color: 'green' }}).addTo(map3)
-
-
-var fourseasons =  function (feature) {
-  var subject = feature.properties.MED_AGE // get the current state's Median Age attribute
-  var newcolor = 'olive' // let the initial color be a darker green
-  if (subject < 38) { newcolor = 'green' } // if the state's median age is less than the average, color it a lighter green
-  return {
-    color: newcolor, // use the color variable above for the value
-    weight: 1,
-    fillOpacity: 0.2
-  }
+var onEachFeatureFunction = function(feature, layer) {
+  // contents of the function
+  var name = feature.properties.STATE_NAME
+  var age = feature.properties.MED_AGE
+  layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+  // statesLayerObject.addLayer(layer)
 }
-
-var spring = { style: fourseasons }
-L.geoJSON(data, spring).addTo(map3)
-
-
-
-
-var age_by_state = function (feature, layer) {
-   var name = feature.properties.STATE_NAME
-   var age = feature.properties.MED_AGE
-   layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
- }
-
-#if ( stateName == 'Louisiana' ) { // code here }
-
-
