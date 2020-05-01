6d21031cb4e2853b278b09e6c0ca9f4537e21061
MosheQ <63529216+MosheQ@users.noreply.github.com>
2020-05-01 12:21:15 -0500

    Update map3.js

--- map 4/map3.js
+++ map 4/map3.js
@@ -8,16 +8,21 @@ var gjsonUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_de
 jQuery.getJSON(gjsonUrl, function(data) {
 
 var styleFunction = function(feature) {
-  var medAge = feature.properties.MED_AGE // get the current state's Median Age attribute
+  var POP = feature.properties.POPULATION // get the current state's Median Age attribute
   var colorString = 'purple' // let the initial color be a darker green
 
-      if (medAge < 25) {
+      if (POP < 1000000) {
     colorString = 'red'
   } // if the state's median age is less than the average, color it a lighter green
 
-  if (medAge < 38) {
-    colorString = 'green'
+ if ((POP > 1000000) && (POP < 10000000)) {
+    colorString = 'olive'
   } // if the state's median age is less than the average, color it a lighter green
+  
+  if (POP > 10000000) {
+    colorString = 'Green'
+  } // if the state's median age is less than the average, color it a lighter green
+  
   return {
     color: colorString, // use the color variable above for the value
     weight: 1,
@@ -35,7 +40,7 @@ L.geoJSON(data, geojsonObject).addTo(mymap3)
 var onEachFeatureFunction = function(feature, layer) {
 // contents of the function
 var name = feature.properties.STATE_NAME
-var age = feature.properties.MED_AGE
-layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+var Population = feature.properties.POPULATION
+layer.bindPopup('Population of ' + name + ': ' + Population + '<br>National average: 6.46 million')
 // statesLayerObject.addLayer(layer)
 }
